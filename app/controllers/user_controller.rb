e1b7aae468065a14bbc31ab0651d23f3fbe7c6df
kaid-0 <darrenlewisworks@gmail.com>
2023-10-31 19:28:44 +0530

    Add flash messages to Views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -34,6 +34,7 @@ class UserController < ApplicationController
 
 	def signin_create
 		user = User.find_by(email: params[:email])
+		
 		if user.present? && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			redirect_to root_path, notice: "Logged in successfully!"
