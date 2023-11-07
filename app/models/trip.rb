798784faf348d6d027fb964d32be1626a45252ed
kaid-0 <darrenlewisworks@gmail.com>
2023-11-07 13:17:41 +0530

    Add validations for Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,9 @@
 class Trip < ApplicationRecord
 	belongs_to :user
+
+	validates :user_id, presence: true
+	validates :city, presence: true
+	validates :from_date, presence: true
+	validates :end_date, presence: true
+	validates :head_count, presence: true, numericality: { only_integer: true }
 end
